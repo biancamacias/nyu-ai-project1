41e81040ccef69c2f0d00bff166776787e75e449
biancamacias <bimacias14@gmail.com>
2021-11-04 16:41:31 -0400

    added functionality to main with helper functions

--- AI_puzzle.py
+++ AI_puzzle.py
@@ -2,18 +2,21 @@
 # Isabel Huey, Bianca Macias
 # ijh234, bm2815
 
-WEIGHTS = [1, 1.2, 1.4]
+WEIGHTS = [1.0, 1.2, 1.4]
+DIRECTIONS = ["UP", "DOWN", "LEFT", "RIGHT"]
 
 # graph search will use this Node data structure throughout
 class Node:
    def __init__(self, state = 0, g = 0, goal_state, weight):
       self.state = state # list, current state of puzzle
       self.parent = None # None if root, must be another node otherwise
-      self.direction = None # TODO: maybe don't need this? we'll see
 
       self.g = g # node level
-      self.h = self.calculate_h(goal_state) # Manhattan distances from goal state
-      self.f = self.calculate_f(weight) # f(n) = weight * h(n) + g(n)
+      self.h = 0 # manhattan distances from goal state, will be 0 when goal is reached
+      self.f = 0 # f(n) = weight * h(n) + g(n)
+
+      self.calculate_h(goal_state)
+      self.calculate_f(weight)
 
     def calculate_h(self, goal_state):
         # calculates manhattan distances of curr state from goal state
@@ -23,7 +26,12 @@ class Node:
                 goal_element = goal_state[goal_row][goal_column]
                 curr_row, curr_column = find_element(goal_element)
                 total += abs(goal_row - curr_row) + abs(goal_column - curr_column)
-        return total
+        self.h = total
+
+    def calculate_f(self, weight):
+        # calculates f value
+        # returns f value
+        self.f = weight * self.h + self.g
 
     def find_element(self, element):
         # finds row, column of element in the current state
@@ -33,48 +41,94 @@ class Node:
                 if self.state[row][column] == element:
                     return row, column
 
-    def calculate_f(self, weight):
-        # calculates f value
-        return weight * self.h + self.g
+    def find_empty_tile(self):
+        # TODO: create helper function to find empty tile position
+        # returns tuple (row, column)
 
-# create helper fucnction can if can move in each direction
+    def move_possible(self, direction):
+        # TODO: create helper fucnction can if can move in each direction
+        # takes direction (from DIRECTIONS list)
+        # returns True or False if tile can move in that direction
 
-# create helper function to check if it is a repeated state
+def best_node(child_nodes):
+    # TODO: create helper function to decide which child node is best based on f value
+    # returns tuple (direction, node with lowest f value)
 
-def move(empty, state, rowLoc, colLoc, wValue, goalState):
-    # Need to check if there is a tile in each direction around empty tile space
-    up = rowLoc - 1
-    down = rowLoc + 1
-    left = colLoc + 1
-    right = colLoc -1
+def move_up(node, g):
+    # TODO: helper function that takes current node and creates new node where tile moves up
+    # returns new node with node as its parent
 
-    # Check if there is a value to move for each direction then create a node
-    # Call function to calculate new f(n) for each node
-    # Need to update state and keep count of nodes
-    if (0<up<len(state)):
-        up = Node(state[up][colLoc])
-    if (0<down<len(state)):
-        down = Node(state[down][colLoc])
-    if (0<left<len(state[0])):
-        left = Node(state[left][rowLoc])
-    if (0<right<len(state[0])):
-        right = Node(state[right][rowLoc])
+def move_down(node, g):
+    # TODO: helper function that takes current node and creates new node where tile moves down
+    # returns new node with node as its parent
 
+def move_left(node, g):
+    # TODO: helper function that takes current node and creates new node where tile moves left
+    # returns new node with node as its parent
+
+def move_right(node, g):
+    # TODO: helper function that takes current node and creates new node where tile moves right
+    # returns new node with node as its parent
+
+def generate_children(node, new_up, new_down, new_left, new_right, generated_states):
+    # helper function that generates valid children based on directions they can move in
+    # if new child state has already been generated before, skip it (no repeated states allowed)
+    # if not, add to generated_states and children list
+    # returns children
+    # value of children will be None if direction is not allowed
+    new_up = None
+    new_down = None
+    new_left = None
+    new_right = None
+    if node.move_possible(DIRECTIONS[0]):
+        new_up = move_up(node, g)
+        if new_up.state not in generated_states:
+            generated_states.append(new_up.state)
+        else:
+            new_up = None
+    if node.move_possible(DIRECTIONS[1]):
+        new_down = move_down(node, g)
+        if new_down.state not in generated_states:
+            generated_states.append(new_down.state)
+        else:
+            new_down = None
+    if node.move_possible(DIRECTIONS[2]):
+        new_left = move_left(node, g)
+        if new_left.state not in generated_states:
+            generated_states.append(new_left.state)
+        else:
+            new_left = None
+    if node.move_possible(DIRECTIONS[3]):
+        new_right = move_right(node, g)
+        if new_right.state not in generated_states:
+            generated_states.append(new_right.state)
+        else:
+            new_right = None
+    return [new_up, new_down, new_left, new_right]
+
+def move(node, g, generated_states):
+    # creates nodes if empty tile can move up, down, left, right
+    # and if the node has not yet been created
+    # sets child node parent to node
+    # returns tuple (direction taken, node with best f acc to A* search)
+
+    child_nodes = generate_children(node, generated_states)
+    # [up node or None, down node or None, left node or None, right node or None]
+
+    return best_node(child_nodes)
 
 
 def main():
     # open the file
     file = open("AI_puzzle_test.txt", "r")
-    weight = 1.0 # TODO: change this
+    weight = WEIGHTS[1] # TODO: change this
 
-    # puzzle list data structure: [[row 1], [row 2], [row 3]]
+    # puzzle state data structure: [[row 1], [row 2], [row 3]]
     initial_state = [] # initial state of puzzle
-    state = []
     goal_state = [] # will hold goal state, input from input file
-    index = 0
-    states_created = [] # list to hold states already created
-    breat_at = 0 # TODO: do we use this?
-    g = 0 # g(n) value, root starts at
+    generated_states = [] # list to hold nodes already created to prevent repeated states
+    g = 0 # g(n) value, root starts at 0
+    optimal_path = [] # list of directions taken
 
     # Make a list
     for line in file:
@@ -86,20 +140,25 @@ def main():
     curr_state = initial_state[0:3]
 
     # find initial position of empty tile
-    for row in curr_state:
-        if '0' in line:
-            colLoc = row.index('0')
-            rowLoc = curr_state.index(row)
+    # for row in curr_state:
+    #     if '0' in line:
+    #         colLoc = row.index('0')
+    #         rowLoc = curr_state.index(row)
 
     goal_state = initial_state[4:8]
 
-    # maybe while loop
-    # while state!=goalState:
-    # check if initial state == goal state, then loop
-    # add g counter in while loop
-    # create root, then loop to create children
-    root = Node(curr_state, 0, goal_state, weight) # TODO: data in node should be whole table
-    move(empty, curr_state, rowLoc, colLoc, weight, goal_state) # put this into a loop
+    # create root, then start generating graph tree
+    root = Node(curr_state, 0, goal_state, weight)
+    generated_states.append(root.state)
+    goal_reached = False
+    curr_best_node = root
+    while !(goal_reached):
+        g += 1
+        if curr_best_node.state == goal_state:
+            goal_reached = True
+        else:
+            direction, next_node = move(curr_best_node, g, generated_states)
+            curr_best_node = next_node
 
     file.close()
 
