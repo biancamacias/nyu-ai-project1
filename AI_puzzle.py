a172e76edeaf6a31dec4dd3a77486fa61b86c5c3
izzyjh <ijh234@nyu.edu>
2021-11-08 22:22:49 -0500

    cleaned up a bit

--- AI_puzzle.py
+++ AI_puzzle.py
@@ -3,31 +3,32 @@
 # ijh234, bm2815
 import copy
 
-WEIGHTS = [1.0, 1.2, 1.4]
-DIRECTIONS = ["L", "R", "U", "D"]
-
-# graph search will use this Node data structure throughout
+# Node is a unique tile arrangement that hold state, parent node, weight,
+# direction taken to get to the tile arrangement, g, h, and f
 class Node:
-   def __init__(self, weight, goal_state, state, direction = None, g = 0):
-      self.state = state # list, current state of puzzle
-      self.parent = None # None if root, must be another node otherwise
-      self.weight = weight
+    def __init__(self, weight, goal_state, state, direction = None, g = 0):
+        self.state = state # list, current state of puzzle
+        self.parent = None # None if root, must be another node otherwise
+        self.weight = weight
 
-      self.direction = direction
+        self.direction = direction # "L", "R", "U", or "D"
 
-      self.g = g # node level
-      self.h = 0 # manhattan distances from goal state, will be 0 when goal is reached
-      self.f = 0 # f(n) = weight * h(n) + g(n)
+        self.g = g # node level
+        self.h = 0 # manhattan distances from goal state, will be 0 when goal is reached
+        self.f = 0 # f(n) = weight * h(n) + g(n)
 
-      self.calculate_h(goal_state)
-      self.calculate_f(weight)
+        self.calculate_h(goal_state) # call function to calculate h
+        self.calculate_f(weight) # call function to calculate f
 
-   def manhattan_distance(self, goal_row, goal_column, curr_row, curr_column):
-       return abs(goal_row - curr_row) + abs(goal_column - curr_column)
+    # Method to calculate manhattan distances of the current tile
+    # Takes in the goal position and the current position
+    # Returns individual manhattan distance (integer)
+    def manhattan_distance(self, goal_row, goal_column, curr_row, curr_column):
+        return abs(goal_row - curr_row) + abs(goal_column - curr_column)
 
-   def find_element(self, goal_element):
-        # finds row, column of element in the current state
-        # returns tuple of its location (row, column)
+    # Method that finds row, column of element in the current state
+    # returns tuple of its location (row, column)
+    def find_element(self, goal_element):
         for row in self.state:
             for element in row:
                 if element == goal_element:
@@ -35,9 +36,9 @@ class Node:
                     col_index = row.index(element)
                     return row_index, col_index
 
-   def calculate_h(self, goal_state):
-        # calculates manhattan distances of curr state from goal state
-        # sets h value
+    # Method calculates manhattan distances of current state from goal state
+    # sets h attribute
+    def calculate_h(self, goal_state):
         total = 0
         for goal_row in goal_state:
             for goal_element in goal_row:
@@ -45,62 +46,43 @@ class Node:
                 total += self.manhattan_distance(goal_state.index(goal_row), goal_row.index(goal_element), curr_row, curr_column)
         self.h = total
 
-   def calculate_f(self, weight):
-        # calculates and sets f value
+    # Method calculates and sets f attribute
+    def calculate_f(self, weight):
         self.f = round((weight* self.h + self.g) , 1)
 
-
-
-
-
-   def find_empty_tile(self):
-       # TODO: create helper function to find empty tile position
-       # returns tuple (row, column)
-       # find initial position of empty tile
-       for row in self.state:
+    # Method to find empty tile position
+    # returns tuple (row, column)
+    def find_empty_tile(self):
+        for row in self.state:
            for element in row:
                if element == '0':
                    col = row.index('0')
                    row = self.state.index(row)
                    return(row, col)
-       print("Error: Cannot find empty tile")
 
-
-   def move_possible(self, direction, empty_tile):
-        # TODO: create helper fucnction can if can move in each direction
-        # takes direction (from DIRECTIONS list)
-        # returns True or False if tile can move in that direction
+    # Method that checks if empty tile can move in given direction
+    # returns True or False if tile can move in that direction
+    def move_possible(self, direction, d_index, empty_tile):
         (row,col) = empty_tile
         min_row = -1
         min_col = -1
         max_row = len(self.state)
         max_col = len(self.state[0])
 
-        if direction == "U":
-            up = row - 1
-            if min_row < up < max_row :
+        if (direction == "U") | (direction == "D"):
+            if min_row < d_index < max_row :
                 return True
             else: return False
-        if direction == "D":
-            down = row + 1
-            if min_row < down < max_row :
-                return True
-            else: return False
-        if direction == "L":
-            left = col - 1
-            if min_col < left < max_col :
-                return True
-            else: return False
-        if direction == "R":
-            right = col + 1
-            if min_col < right < max_col :
+        if (direction == "L") | (direction == "R"):
+            if min_col < d_index < max_col :
                 return True
             else: return False
 
-def best_node(child_nodes, unexpanded_nodes):
-    # TODO: create helper function to decide which child node is best based on f value
-    # returns tuple (direction, node with lowest f value)
-    # import pdb; pdb.set_trace()
+
+# Helper function to decide which child node is best based on f value
+# The node is best if it has the lowest f value out of all the unexpanded nodes
+# returns tuple (direction, node with lowest f value)
+def best_node(child_nodes, frontier):
     min_f = -1
     direction_moved = None
     node_chosen = None
@@ -113,29 +95,27 @@ def best_node(child_nodes, unexpanded_nodes):
                 direction_moved = direction
                 node_chosen = node
     # Check if unexpanded nodes have a lower f value
-    for unexpanded_node in reversed(unexpanded_nodes):
-        if unexpanded_node.f < node_chosen.f:
-            if unexpanded_node.g < node_chosen.g:
-                node_chosen = unexpanded_node
-    # for unexpanded_node in unexpanded_nodes:
-    #     if unexpanded_node.f < node_chosen.f:
-    #         if unexpanded_node.g < node_chosen.g:
-    #             node_chosen = unexpanded_node
-    # If an unexpanded node value is chosen delete it from unexpanded node list
-    if node_chosen in unexpanded_nodes:
-        unexpanded_nodes.remove(node_chosen)
-    # put generated nodes not chosen in unexpanded nodes list
+    # Iterate through the reverse of the unepanded_nodes list
+    # because A* weighted search has a LIFO Frontier
+    for leaf_node in reversed(frontier):
+        if leaf_node.f < node_chosen.f:
+            if leaf_node.g < node_chosen.g:
+                node_chosen = leaf_node
+    # If an unexpanded node value is chosen delete it from unexpanded_nodes list
+    # because it will now be expanded
+    if node_chosen in frontier:
+        frontier.remove(node_chosen)
+    # Put generated nodes not chosen into unexpanded_nodes list (Frontier)
     for node in child_nodes:
         if node[1]!= None:
             if node[1] is not node_chosen:
-                unexpanded_nodes.append(node[1])
-
-    # compare generated children's f values and unexpanded nodes f values
+                frontier.append(node[1])
     return node_chosen
 
+# Helper function that takes current node, direction, g, empty_tile position, and goal_state
+# and moves the empty tile accordingly
+# It then returns a new node with the current node as its parent
 def move(node, direction, g, empty_tile, goal_state):
-    # TODO: helper function that takes current node and creates new node where tile moves down
-    # returns new node with node as its parent
     new_state = copy.deepcopy(node.state)
     weight = node.weight
     (row, col) = empty_tile
@@ -152,46 +132,50 @@ def move(node, direction, g, empty_tile, goal_state):
     return new_node
 
 
+# Helper function that generates valid children based on directions they can move in
+# if new child state has already been generated before, skip it (no repeated states allowed)
+# if not, add to generated_states and children list
+# returns children list
+# value of children will be None if direction is not allowed
 def generate_children(node, g, generated_states, goal_state):
-    # helper function that generates valid children based on directions they can move in
-    # if new child state has already been generated before, skip it (no repeated states allowed)
-    # if not, add to generated_states and children list
-    # returns children list
-    # value of children will be None if direction is not allowed
     new_up = None
     new_down = None
     new_left = None
     new_right = None
     empty_tile = node.find_empty_tile()
     (row, col) = empty_tile
-    if node.move_possible(DIRECTIONS[0], empty_tile):
-        direction = (DIRECTIONS[0], col - 1)
+    left_index = col - 1
+    right_index = col + 1
+    up_index = row - 1
+    down_index = row + 1
+    if node.move_possible("L", left_index, empty_tile):
+        direction = ("L", left_index)
         new_left = move(node, direction, g, empty_tile, goal_state)
-        new_left.direction = DIRECTIONS[0]
+        new_left.direction = "L"
         if new_left.state not in generated_states:
             generated_states.append(new_left.state)
         else:
             new_left = None
-    if node.move_possible(DIRECTIONS[1], empty_tile):
-        direction = (DIRECTIONS[1], col + 1)
+    if node.move_possible("R", right_index, empty_tile):
+        direction = ("R", right_index)
         new_right = move(node, direction, g, empty_tile, goal_state)
-        new_right.direction = DIRECTIONS[1]
+        new_right.direction = "R"
         if new_right.state not in generated_states:
             generated_states.append(new_right.state)
         else:
             new_right = None
-    if node.move_possible(DIRECTIONS[2], empty_tile):
-        direction = (DIRECTIONS[2], row - 1)
+    if node.move_possible("U", up_index, empty_tile):
+        direction = ("U", up_index)
         new_up = move(node, direction, g, empty_tile, goal_state)
-        new_up.direction = DIRECTIONS[2]
+        new_up.direction = "U"
         if new_up.state not in generated_states:
             generated_states.append(new_up.state)
         else:
             new_up = None
-    if node.move_possible(DIRECTIONS[3], empty_tile):
-        direction = (DIRECTIONS[3], row + 1)
+    if node.move_possible("D", down_index, empty_tile):
+        direction = ("D", down_index)
         new_down = move(node, direction, g, empty_tile, goal_state)
-        new_down.direction = DIRECTIONS[3]
+        new_down.direction = "D"
         if new_down.state not in generated_states:
             generated_states.append(new_down.state)
         else:
